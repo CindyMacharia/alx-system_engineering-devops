91185a7f07e3434642c5d7e877bf7f3bb3d84697
CindyMacharia <cindymash29@gmail.com>
2021-09-02 15:06:48 +0300

    Negative and positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include "main.h"
+
+/**
+  * o-main - tests function that prints if integer is positive or negative
+  * Return: 0
+  */
+
+int main(void)
+{
+
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
